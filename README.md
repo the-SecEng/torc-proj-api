fe5fc58dfe66613b2efae760785e13c8de372874
Goktug Erol <goktug.erol@machsoft.cloud>
2024-03-25 21:23:57 -0500

    Add project files.

---
+++ README.md
@@ -0,0 +1,2 @@
+# torc-proj
+ The project includes an API and a simple frontend app.
---
+++ Torc Project - API/DbContexts/BookLibraryContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Torc_Project___API.DbContexts
+{
+    public class BookLibraryContext:DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("###");
+        }
+    }
+}
